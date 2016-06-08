132feb4150f018415b177458753b155465cc319e
Chris Sprague <chris@chaoscontrol.org>
2016-06-08 18:55:07 -0400

    add password reset and redirect routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,32 @@
 <?php
 
+// --- Public Routes
+
 // Main page
 Route::get('/', function () {
     return view('welcome');
 });
 
 // User Authentication
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('user/login', 'Auth\AuthController@getLogin');
+Route::post('user/login', 'Auth\AuthController@postLogin');
 
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('user/logout', 'Auth\AuthController@getLogout');
 
 // Account Registration
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('user/register', 'Auth\AuthController@getRegister');
+Route::post('user/register', 'Auth\AuthController@postRegister');
+
+// Password reset request
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+// --- Authenticated Routes
+
+Route::get('user', ['middleware' => 'auth', function() {
+    // redirect to /user/login for now
+}]);
