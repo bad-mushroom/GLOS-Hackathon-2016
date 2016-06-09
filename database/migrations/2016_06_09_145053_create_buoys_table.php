8a7559e29d46d51e29d32bbf3ce0dcc81dd587f7
Chris Sprague <chris@chaoscontrol.org>
2016-06-09 18:15:39 -0400

    update migration to account for null values

--- database/migrations/2016_06_09_145053_create_buoys_table.php
+++ database/migrations/2016_06_09_145053_create_buoys_table.php
@@ -14,15 +14,16 @@ class CreateBuoysTable extends Migration
     {
         Schema::create('buoys', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('buoyId');
             $table->string('lastDataUpdate');
             $table->decimal('zValue', 3, 2);
             $table->string('shortName', 25);
-            $table->string('longName', 100);
+            $table->string('longName', 100)->nullable();
             $table->string('stationUrl')->nullable();
             $table->string('state', 2);
-            $table->string('ndbcHandler');
+            $table->string('ndbcHandler')->nullable();
             $table->string('imageUrl')->nullable();
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->boolean('active');
             $table->timestamps();
         });
