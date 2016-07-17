1b1ded28327f7972aedf61acf320eec9302fe0e2
Chris Sprague <chris@chaoscontrol.org>
2016-07-17 08:05:13 -0400

    add dashboard migration and seed

---
+++ database/migrations/2016_07_17_113222_create_dashboards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDashboardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dashboards', function (Blueprint $table) {
+            // PK
+            $table->increments('id');
+            // Name
+            $table->string('name', 50)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dashboards');
+    }
+}
