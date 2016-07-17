8f7f08ea369af1396f172bdd646247e60c611f8a
Chris Sprague <chris@chaoscontrol.org>
2016-07-17 18:00:10 -0400

    add artisan command for updating sensors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,25 +7,28 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        Commands\UpdateBuoy::class,
-        Commands\ListBuoy::class
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		Commands\UpdateBuoy::class,
+		Commands\UpdateSensors::class,
+		Commands\ListBuoy::class
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // Run the task daily at 1:00 & 13:00
-        $schedule->command('buoys:update')->twiceDaily(1, 13)->withoutOverlapping();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		// Run the task daily at 1:00 & 13:00
+		$schedule->command('buoys:update')->twiceDaily(1, 13)->withoutOverlapping();
+
+		// $schedule->command('buoys:sensorsUpdate')->onceMonthly(1, 13)->withoutOverlapping();
+	}
 }
