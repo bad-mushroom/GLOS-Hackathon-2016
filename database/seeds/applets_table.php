897808228103c772cc7651329ae0552c46e25218
Chris Sprague <chris@chaoscontrol.org>
2016-07-17 17:59:35 -0400

    add migrations for sensors, update seeds

--- database/seeds/applets_table.php
+++ database/seeds/applets_table.php
@@ -4,35 +4,39 @@ use Illuminate\Database\Seeder;
 
 class applets_table extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('applets')->insert([
-            'id'        => 1,
-            'shortname' => 'waveheight',
-            'fullname'  => 'Wave Height',
-        ]);
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('applets')->insert([
+			'id'                 => 1,
+			'shortname'          => 'waveheight',
+			'fullname'           => 'Wave Height',
+			'distance_range'     => 50,
+		]);
 
-        DB::table('applets')->insert([
-            'id'        => 2,
-            'shortname' => 'watertemp',
-            'fullname'  => 'Water Temperature',
-        ]);
+		DB::table('applets')->insert([
+			'id'                 => 2,
+			'shortname'          => 'watertemp',
+			'fullname'           => 'Water Temperature',
+			'distance_range'     => 50,
+		]);
 
-        DB::table('applets')->insert([
-            'id'        => 3,
-            'shortname' => 'watervelocity',
-            'fullname'  => 'Water Velocity',
-        ]);
+		DB::table('applets')->insert([
+			'id'                 => 3,
+			'shortname'          => 'watervelocity',
+			'fullname'           => 'Water Velocity',
+			'distance_range'     => 50,
+		]);
 
-        DB::table('applets')->insert([
-            'id'        => 4,
-            'shortname' => 'windspeed',
-            'fullname'  => 'Wind Knots',
-        ]);
-    }
+		DB::table('applets')->insert([
+			'id'                 => 4,
+			'shortname'          => 'windspeed',
+			'fullname'           => 'Wind Knots',
+			'distance_range'     => 50,
+		]);
+	}
 }
